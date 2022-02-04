7e4876fe3359ee37178c80f0d844db53c7286092
sambatika1 <milak.alqathami@gmail.com>
2022-02-04 14:13:16 +0300

    .🧸

---
+++ horsi-front/horsi/src/Checkout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Checkout() {
+  return <div className="checkout">
+      
+  </div>;
+}
+
+export default Checkout;
--- horsi-front/horsi/src/Header.js
+++ horsi-front/horsi/src/Header.js
@@ -3,12 +3,19 @@ import './Header.css'
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined';
+import {Link} from "react-router-dom";
 function Header() {
   return <div className="header">
-      <img  className="header_logo"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmA3NIwfVieB0sNbdj2XsX7nn9dAxmlbK9cw&usqp=CAU"/>
+    <Link to="/">
+    <img  className="header_logo"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmA3NIwfVieB0sNbdj2XsX7nn9dAxmlbK9cw&usqp=CAU"/>
       
 
+    </Link>
+
+     
+<Link to="/checkout">
 <LocalMallOutlinedIcon fontSize="large" className="header_icon"/>
+</Link>
 <FavoriteBorderOutlinedIcon fontSize="large" className="header_icon"/>
 <PersonOutlineOutlinedIcon fontSize="large" className="header_icon"/> 
 
