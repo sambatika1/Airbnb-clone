98a64a64bc39310d78a508aedefa5575835f202d
milak <milak.alqathami@gmail.com>
2022-05-17 09:43:03 +0300

    HOME

---
+++ airbnb/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Home from './Home';
+import Header from './Header'
+import Footer from "./Footer"
+function App() {
+  return (
+    <div className="app">
+   
+      <Header/>
+      <Home/>
+        {/*home*/}
+      {/*  Header*/ }
+       {/* Banner*/}
+       {/* search*/}
+       {/* Cards*/}
+  <Footer/>
+       {/* SearchPage*/}
+
+
+    </div>
+  );
+}
+
+export default App;
